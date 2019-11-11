c62e039607e225d2aef74bfca9db7afb7cfaa824
wwlee94 <38131683+wwlee94@users.noreply.github.com>
2019-11-11 15:26:31 +0900

    Docker Run Test 추가

--- app/example/python/basic.py
+++ app/example/python/basic.py
@@ -4,15 +4,13 @@
 # 다음과 같이 import를 사용할 수 있습니다.
 # import math
 
-def solution(cookings, n):
+def solution(n):
     # 여기에 코드를 작성해주세요.
-    answer = 0
+    answer = n
     return answer
 
 # 아래는 테스트케이스 출력을 해보기 위한 코드입니다.
-cookings = [[4,3000],[3,4000]]
-n = 5
-ret1 = solution(cookings, n)
+ret1 = solution(100)
 
 # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은 "+ str(ret1)+ " 입니다.")
---
+++ app/userfile/user_26.py
@@ -0,0 +1,4 @@
+# 메인함수
+if __name__ == '__main__':
+    while(True):
+        print("Hello world")
